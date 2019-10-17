2cbec8abab763275213fc901b8c2e39c1da17498
ryamaguti07 <renan.o.yamaguti@gmail.com>
2019-10-17 01:40:26 -0300

    Update computeCost.m

--- computeCost.m
+++ computeCost.m
@@ -1,12 +1,9 @@
 function [J, grad] = computeCost(theta, X, y, lambda)
-  m = length(y); % number of training examples
+  m = length(y);
     h = sigmoid(X * theta);
     J = (y' * log(h) + (1 - y)' * log(1 - h)) / -m;
     grad = X' * (h - y) / m;
- 
-    % Regularization
     th = theta; th(1) = 0;
- 
     J = J + th' * th * lambda / m / 2;
     grad = grad + th * lambda / m;
 
